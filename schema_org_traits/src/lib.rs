6c4c367be8d17dc6cbf36b7f235b84d5cd11cc27
Thomas Gauges <thomas.gauges@gmail.com>
2024-03-18 23:24:05 +0100

    doc: add doc_auto_cfg attribute

--- schema_org_traits/src/lib.rs
+++ schema_org_traits/src/lib.rs
@@ -1,3 +1,6 @@
+// https://doc.rust-lang.org/rustdoc/unstable-features.html#doc_auto_cfg-automatically-generate-doccfg
+#![cfg_attr(doc, feature(doc_auto_cfg))]
+
 #[cfg(any(feature = "json-ld_0_15", doc))]
 pub mod json_ld_0_15;
 mod schemas;
